175886d4a842f07d11da276c208e53cee3fe1ffa
Hooram Nam <hooram@podri.org>
2018-06-07 23:25:34 +0900

    attempt to fix scalefactor when turning sidebar on/off

--- src/react-image-lightbox.js
+++ src/react-image-lightbox.js
@@ -67,7 +67,7 @@ class ReactImageLightbox extends Component {
     if (width > windowWidth) {
       nextX += (windowWidth - width) / 2;
     }
-    const scaleFactor = zoom * (targetWidth / width);
+    const scaleFactor = zoom * ((targetWidth-sidebarWidth) / width);
 
     return {
       transform: `translate3d(${nextX}px,${y}px,0) scale3d(${scaleFactor},${scaleFactor},1)`,
